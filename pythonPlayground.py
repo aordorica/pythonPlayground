023b12a36b9158c48cdfc8c8e1346fc0a643b44f
Alan Ivan Ordorica <alano@geralt.lan>
2020-12-12 13:57:32 -0800

    Initial commit

---
+++ pythonPlayground.py
@@ -0,0 +1,8 @@
+p = "The quick brown fox jumps over the ;azy dog"
+
+letters =  sorted(p)
+
+nums = [104, 113, 128, 101, 54, 189, 9, 210, 190, 191]
+
+for index, value in enumerate(sorted(nums)):
+    print(index, value)
